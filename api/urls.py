a36f8fd9e1379fc4ae8c22fef0e0798ea13e2e77
Franz Genegobis <genegobis.franz@gmail.com>
2024-03-27 17:48:12 +0800

    Modify urls.py

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('user/', views.getData),
-  path('user/<int:id>', views.getUser),
+  path('', views.getData)
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,3 @@ from django.http import JsonResponse
 def getData(request):
     person = {'name':'MetroEvents API Deploy Test', 'age':20}
     return JsonResponse(person)
-
-
-def getUser(request, id):
-    
