ac110e3822e793dd1b39711a5a981908c6550334
Franz Genegobis <genegobis.franz@gmail.com>
2024-03-28 15:22:55 +0800

    Finally fixed the nested serialization and added a temporary Event model..

--- api/models.py
+++ api/models.py
@@ -2,8 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
     isOrganizer = models.BooleanField(default=False)
   
     def __str__(self):
         return self.user.username
+
+class Event(models.Model):
+    eventName = models.TextField(null=True)
+    eventVenue = models.TextField(null=True)
+    eventDate = models.DateTimeField(auto_now=True)
+    eventDescription = models.TextField(null=True)
+    eventNumberOfAttendees = models.IntegerField(default=0)
+    eventLikes = models.IntegerField(default=0)
+    eventOrganizer = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
--- api/views.py
+++ api/views.py
@@ -1,9 +1,42 @@
-from .models import UserProfile
-from django.views.decorators.http import require_POST
-from .serializers import UserProfileSerializer, UserSerializer
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+
+from .models import Event
+from .serializers import RegisterUserSerializer, UserSerializer, EventSerializer, UserProfileSerializer
+
+@api_view(["POST"])
+def validate_register(request):
+    if request.method == "POST":
+        user_serializer = RegisterUserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user = user_serializer.save()
+            return Response(user_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(user_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"error": "Method not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+@api_view(['POST', 'GET'])
+def validate_login(request):
+    if request.method == "POST":
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            serializer = UserSerializer(user)
+            return Response({"message": "Login Successful"}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def org_homepage(request):
+    if request.method == "GET":
+        events = Event.objects.all()
+        event_serializer = EventSerializer(events, many=True)
+        return Response({'events': event_serializer.data})
 
 
 
@@ -11,15 +44,3 @@ from rest_framework import status
 def getData(request):
     person = {'name':'MetroEvents API Deploy Test', 'age':20}
     return Response(person)
-
-
-@api_view(["POST"])
-def validate_register(request):
-    if request.method == "POST":
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    return Response({"error": "Method not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-
