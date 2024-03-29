420f3806d39cbb0de0594c5e557556578f9fcd61
HoneyHanny <hansduran25@gmail.com>
2024-03-29 08:32:25 +0800

    Change endpoints

--- api/urls.py
+++ api/urls.py
@@ -7,13 +7,13 @@ urlpatterns = [
   path('login/', views.validate_login),
   path('homepage/', views.org_homepage), # get all events
 
-  path('event/<int:id>/', views.get_event),
+  path('event/get/<int:id>/', views.get_event),
   path('event/add/', views.add_event),
   path('event/update/<int:id>/', views.update_event),
   path('event/remove/<int:id>/', views.remove_event),
 
-  path('users/', views.get_all_users),
-  path('user/<int:id>/', views.get_user),
+  path('user/get/', views.get_all_users),
+  path('user/get/<int:id>/', views.get_user),
   path('user/update/<int:id>/', views.update_user),
   path('user/remove/<int:id>/', views.remove_user),
 ]
