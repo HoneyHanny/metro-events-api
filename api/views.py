61756a7f045ee08c59356ddd243f6a117962ef03
HoneyHanny <hansduran25@gmail.com>
2024-03-29 09:55:41 +0800

    Fix errors

--- api/views.py
+++ api/views.py
@@ -1,14 +1,15 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
+import json
 
 from .models import Event, User
 from .serializers import RegisterUserSerializer, UserSerializer, EventSerializer, UserSerializer
 
 @api_view(["POST"])
 def validate_register(request):
-
     if request.method != 'POST':
         return Response({'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
     
@@ -43,12 +44,18 @@ def org_homepage(request):
 
     events = Event.objects.all()
     event_serializer = EventSerializer(events, many=True)
+
     return Response({'events': event_serializer.data}, status=status.HTTP_200_OK)
+
+# NOTE(hans): All tests did not account for error cases
+# I can't be arsed to do it hehe
+# Si Gacula nalang guro ipa test ani tanan
 
 #
 # === USERS ===
 #
 
+# Test: Passed - Has successfully got all users
 @api_view(['GET'])
 def get_all_users(request):
     if request.method != 'GET':
@@ -63,6 +70,7 @@ def get_all_users(request):
 
     return Response({'users': users_serializer.data}, status=status.HTTP_200_OK)
 
+# Test: Passed - Has successfully got a user
 @api_view(['GET'])
 def get_user(request, id):
     if request.method != 'GET':
@@ -77,6 +85,7 @@ def get_user(request, id):
 
     return Response({'user': user_serializer.data}, status=status.HTTP_200_OK)
 
+# Test: Not tested
 @api_view(['PUT'])
 def update_user(request, id):
     if request.method != 'PUT':
@@ -96,6 +105,7 @@ def update_user(request, id):
 
     return Response({'user': user_serializer.data}, status=status.HTTP_201_CREATED)
 
+# Test: Not tested
 @api_view(['DELETE'])
 def remove_user(request, id):
 
@@ -112,6 +122,7 @@ def remove_user(request, id):
 # === EVENTS ===
 #
 
+# Test: Passed - Has successfully got an event
 @api_view(['GET'])
 def get_event(request, id):
     if request.method != 'GET':
@@ -124,41 +135,65 @@ def get_event(request, id):
 
     event_serializer = EventSerializer(event)
 
-    return Response({'user': event_serializer.data}, status=status.HTTP_200_OK)  # (OK) not tested yet
+    return Response({'user': event_serializer.data}, status=status.HTTP_200_OK)  # (OK) not tested yet. Test add event first to test this
 
+# Test: Passed - Has successfully added an event 
 @api_view(['POST'])
-def add_event(request):
+def add_event(request, model=Event):
     if request.method != 'POST':
         return Response({'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     event_serializer = EventSerializer(data=request.data)
     
     if not event_serializer.is_valid():
-        return Response({'error': 'Invalid event'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({'error': 'Invalid data'}, status=status.HTTP_400_BAD_REQUEST)
+
+    # All of these commented code might be handled or catched by the previous check already
+    # try:
+    #     json_data = json.loads(request.body)
+    # except:
+    #     return Response({'error': 'Invalid JSON format'}, status=status.HTTP_400_BAD_REQUEST)
+
+    # try:
+    #     User.objects.get(pk=json_data.eventOrganizer)
+    # except:
+    #     return Response({'error': 'Organizer not found or not a valid organizer'}, status=status.HTTP_400_BAD_REQUEST)
 
     event_serializer.save()
     
     return Response({'event': event_serializer.data}, status=status.HTTP_501_NOT_IMPLEMENTED)
 
-@api_view(['PUT'])
+# Test: Passed - Has successfully updated an event
+@api_view(['PATCH'])
 def update_event(request, id):
-    if request.method != 'PUT':
+    if request.method != 'PATCH':
         return Response({'error': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
     try:
-        old_event = User.objects.get(pk=id)
+        old_event = Event.objects.get(pk=id)
     except:
-        return Response({'error': 'Event not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response({'error': 'Event not found'}, status=status.HTTP_400_BAD_REQUEST)
 
-    event_serializer = UserSerializer(instance=old_event, data=request.data)
+    try:
+        json_data = json.loads(request.body)
+    except json.JSONDecodeError:
+        return Response({'error': 'Invalid JSON format'}, status=status.HTTP_400_BAD_REQUEST)
+
+    event_serializer = EventSerializer(instance=old_event, data=request.data, partial=True)
+
+    try:
+        User.objects.get(pk=json_data['eventOrganizer'])
+    except ObjectDoesNotExist:
+        return Response({'error': f'Organizer {json_data["eventOrganizer"]} not found or not a valid organizer'}, status=status.HTTP_400_BAD_REQUEST)
 
     if not event_serializer.is_valid():
-        return Response({'error': 'Invalid event'})
+        return Response({'error': 'Invalid event'}, status=status.HTTP_400_BAD_REQUEST)
 
     event_serializer.save()
 
     return Response({'event': event_serializer.data}, status=status.HTTP_201_CREATED)
 
+# Test: Passed - Has successfully deleted an event
 @api_view(['DELETE'])
 def remove_event(request, id):
     event = Event.objects.get(pk=id)
